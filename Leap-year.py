7d53bb69e24411bb57c0258fe175b7335a2871c1
Melaku <111016250+meliance@users.noreply.github.com>
2022-12-13 23:46:13 +0300

    Create Leap-year.py

---
+++ Leap-year.py
@@ -0,0 +1,10 @@
+if year % 4 ==0:
+      if year % 100 ==0:
+          if year % 400 ==0:
+              leap=True
+          else:
+              leap =False
+  return leap
+
+year = int(input())
+print(is_leap(year))
