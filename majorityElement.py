417fb0645ff21ad922cff1d6a0db3e3ea016698f
Meliance <melakmu66@gmail.com>
2023-12-11 22:34:22 +0300

    Given an array nums of size n, return the majority element.The majority element is the element that appears more than n / 2 times. You may assume that the majority element always exists in the array.

--- majorityElement.py
+++ majorityElement.py
@@ -8,3 +8,15 @@ class Solution:
         
         if counter[majority_element] > len(nums) // 2:
             return majority_element
+            
+        # count =0 
+        # res = 0
+
+        # for num in nums:
+        #     if count == 0:
+        #         res = num
+        #     if num == res:
+        #         count += 1
+        #     else:
+        #         count -= 1
+        # return res
